8757d54e4ec7610e38cf896b1c492ced8eeef9c2
Satish-mourya <satishmourya.o970@gmail.com>
2025-03-23 19:32:49 +0530

    making page responsive

--- fronted/src/pages/home/Home.jsx
+++ fronted/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/header/Header'
 import Exploremenu from '../../components/Exploremenu/Exploremenu'
 import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
+import AppDownload from '../../components/appdownload/AppDownload'
  
 
 
@@ -15,6 +16,7 @@ const Home = () => {
       <Header/>
       <Exploremenu Category={Category} setCategory={setCategory}/>
       <FoodDisplay Category={Category} setCategory={setCategory}/>
+      <AppDownload/>
     </div>
   )
 }
