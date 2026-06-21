ce7da1ceb2977cbf79733248df4633db53627bc8
satish mourya <satishmourya.o970@gmail.com>
2026-06-21 20:59:10 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 
 export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://satishmourya:satish@cluster0.5jlxb1o.mongodb.net/food-delivery').then(()=>{
+    await mongoose.connect('mongodb+srv://satishmourya:Zwads2n2tsbKWuGr@cluster0.5jlxb1o.mongodb.net/food-delivery').then(()=>{
         console.log("DB connected");
     })
 }
